8b72efa127dfde3b761c8f6a65bc9a32145f8977
mahmoudGeoking2022 <mahmoud.ahmed@geoking.com>
2022-08-27 22:35:51 +0200

    Implement rendering view

--- core/Application.php
+++ core/Application.php
@@ -6,15 +6,17 @@ namespace app\core;
 
 class Application
 {
+  public static string $ROOT_DIR;
   public Router $router;
   public Request $request;
   public Response $response;
 
-  public function __construct()
+  public function __construct($rootDir)
   {
+    self::$ROOT_DIR = $rootDir;
     $this->request = new Request();
     $this->response = new Response();
-        $this->router = new Router($this->request, $this->response);
+    $this->router = new Router($this->request, $this->response);
   }
 
   public function run()
